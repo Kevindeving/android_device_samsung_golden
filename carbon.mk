b33c54b5b61cd1ac0b4c85246bb837bc2b4d0e3a
Kevindeving <98kevchen@gmail.com>
2014-02-06 15:02:13 +0100

    Initial carbon bringup

--- carbon.mk
+++ carbon.mk
@@ -15,16 +15,28 @@
 # limitations under the License.
 #
 
-# Inherit CM common GSM/Phone stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := golden
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Inherit some Carbon common stuff.
+$(call inherit-product, vendor/cm/config/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/golden/golden.mk)
 
+# languages
+PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
+
 # Device identifier
 PRODUCT_DEVICE := golden
-PRODUCT_NAME := cm_golden
+PRODUCT_NAME := carbon_golden
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I8190
 PRODUCT_MANUFACTURER := samsung
